36bf15fdd60693e67f32072327ed95ca7827c362
Gerson2102 <gersonloavas@gmail.com>
2026-02-04 17:19:04 -0600

    Fixing vulnerabilities in deps

--- components/leaderboard/index.ts
+++ components/leaderboard/index.ts
@@ -3,7 +3,6 @@ export { RankingsTableRow } from "./RankingsTableRow";
 export { RankingsCard } from "./RankingsCard";
 export { RankingsTable } from "./RankingsTable";
 export { FilterBar } from "./FilterBar";
-export { LeaderboardStats } from "./LeaderboardStats";
 export { LeaderboardContent } from "./LeaderboardContent";
 
 // Insights Dashboard components
